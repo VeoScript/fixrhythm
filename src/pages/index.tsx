2164e4d1dbf7f353d1d33155098db969d5785dce
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-17 15:00:04 +0800

    feat: add default layout (#4)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import React from 'react'
 import Head from 'next/head'
+import Layout from '~/layouts/default'
+import NavigationBar from '~/components/NavigationBar'
 
 const Home: NextPage = () => {
   return (
@@ -8,9 +10,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Fixrhythm</title>
       </Head>
-      <div className="flex flex-col items-center justify-center w-full h-screen">
-        <h1 className="font-normal text-2xl">Welcome to <span className="font-bold">Fixrhythm</span></h1>
-      </div>
+      <Layout>
+        <div className="flex flex-col w-full">
+          <NavigationBar />
+          <h1 className="font-normal text-2xl">Welcome to <span className="font-bold">Fixrhythm</span></h1>
+        </div>
+      </Layout>
     </React.Fragment>
   )
 }
