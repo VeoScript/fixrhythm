a2bf233a3a1681917d68a0e14a5c000a82ce52a5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-17 14:01:30 +0800

    chore: update index page (#3)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>Parky</title>
+        <title>Fixrhythm</title>
       </Head>
       <div className="flex flex-col items-center justify-center w-full h-screen">
-        <h1 className="font-normal text-2xl">Welcome to <span className="font-bold">Parky</span></h1>
+        <h1 className="font-normal text-2xl">Welcome to <span className="font-bold">Fixrhythm</span></h1>
       </div>
     </React.Fragment>
   )
