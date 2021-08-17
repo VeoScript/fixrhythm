cad9f215490fda3496c0ae02709ce3f560682ef8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-17 15:44:06 +0800

    feat: add default colors (#5)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      colors: {
+        'pantone-black': '#24282B', //PANTONE 426 XGC
+        'pantone-white': '#EDF1FF', // PANTONE 11-4001 TCX Brilliant White
+        'pantone-red': '#C71F2D', // PANTONE 18-1763 TCX High Risk Red
+      },
       fontFamily: {
         poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
         ubuntu: ['Ubuntu', ...defaultTheme.fontFamily.sans]
