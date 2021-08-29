316c5ed38b14788256768ebf02a809b16672d0cb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-29 21:25:01 +0800

    feat: add card component (#7)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,13 @@
 import '~/styles/tailwind.css'
+import NextNProgress from '~/lib/NextProgressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import MainLayout from '~/layouts/main'
+import Card from '~/components/Card'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
       <MainLayout>
         <div className="flex flex-col w-full h-full overflow-hidden">
-          <div className="flex flex-row items-center justify-between w-full px-5 py-3 bg-[#202426]">
+          <div className="flex flex-row items-center justify-between w-full px-5 py-3 bg-[#202426] border-b border-pantone-white border-opacity-10">
             <span className="font-light text-pantone-white text-opacity-50">News Feed</span>
             <div className="flex flex-row items-center w-full max-w-[15rem] px-3 py-2 space-x-2 text-sm rounded-md bg-pantone-white bg-opacity-10 border border-transparent focus-within:border-pantone-white focus-within:border-opacity-30">
               <svg className="w-5 h-5 text-pantone-white text-opacity-50" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -25,7 +26,7 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col w-full h-full overflow-y-auto">
-            {/* card here... */}
+            <Card />
           </div>
         </div>
       </MainLayout>
