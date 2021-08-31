137a7952dada9ad3912123334200bdd5a0a169a2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-08-31 09:51:24 +0800

    style: change card text-color (#10)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,32 +14,32 @@ export default function Card() {
               <AuthorAvatar src="https://pbs.twimg.com/media/E9IG6lXWUAo4TBf?format=jpg&name=medium" alt="avatar" />
               <div className="flex flex-col">
                 <span className="font-medium text-sm">Roseanne Park</span>
-                <span className="font-light text-xs text-pantone-white text-opacity-30">The Lyricist</span>
+                <span className="font-light text-xs text-pantone-white text-opacity-50">The Lyricist</span>
               </div>
             </div>
             <div className="flex flex-row items-center justify-end w-full space-x-2">
               <button type="button">
-                <AiOutlineHeart className="w-5 h-5 text-gray-500" />
+                <AiOutlineHeart className="w-5 h-5 text-gray-400" />
               </button>
               <button type="button">
-                <AiOutlineEye className="w-6 h-6 text-gray-500" />
+                <AiOutlineEye className="w-6 h-6 text-gray-400" />
               </button>
               <button type="button">
-                <BiBookmark className="w-5 h-5 text-gray-500" />
+                <BiBookmark className="w-5 h-5 text-gray-400" />
               </button>
             </div>
           </div>
           <div className="flex flex-col w-full mt-3">
             <span className="font-medium text-sm">The Title</span>
-            <span className="font-light text-xs text-pantone-white text-opacity-30">
+            <span className="font-light text-xs text-pantone-white text-opacity-80">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora excepturi laboriosam autem, at quibusdam dicta quam in adipisci vel aliquid quos cum asperiores nostrum eveniet quisquam laborum. Voluptatibus, culpa iusto.
             </span>
             <div className="flex flex-row items-center w-full space-x-1 mt-3">
-              <span className="font-light text-[10px] text-pantone-white text-opacity-20">Just Now</span>
-              <span className="font-light text-[10px] text-pantone-white text-opacity-20">&bull;</span>
-              <span className="font-light text-[10px] text-pantone-white text-opacity-20">0 hearts</span>
-              <span className="font-light text-[10px] text-pantone-white text-opacity-20">&bull;</span>
-              <span className="font-light text-[10px] text-pantone-white text-opacity-20">0 comments</span>
+              <span className="font-light text-[10px] text-pantone-white text-opacity-50">Just Now</span>
+              <span className="font-light text-[10px] text-pantone-white text-opacity-50">&bull;</span>
+              <span className="font-light text-[10px] text-pantone-white text-opacity-50">0 hearts</span>
+              <span className="font-light text-[10px] text-pantone-white text-opacity-50">&bull;</span>
+              <span className="font-light text-[10px] text-pantone-white text-opacity-50">0 comments</span>
             </div>
           </div>
         </div>
