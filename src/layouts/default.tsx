18f791a36c82f2807a8ed30a78a022c2ddd998f0
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-03 17:57:44 +0800

    chore: add preventcopy class

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -9,7 +9,7 @@ interface TypeProps {
 
 const Layout: React.FC<TypeProps> = ({ children }) => {
   return (
-    <div className="font-poppins flex justify-center w-full h-screen bg-[#222] overflow-hidden">
+    <div className="preventcopy font-poppins flex justify-center w-full h-screen bg-pantone-darkblack overflow-hidden">
       <div className="relative flex flex-col w-full max-w-[2400px] h-full text-pantone-white bg-pantone-black bg-opacity-50">
         <NavigationBar />
         <div className="flex flex-row items-start w-full max-w-full h-full">
