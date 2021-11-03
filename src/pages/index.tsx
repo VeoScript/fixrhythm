b24a322b78bd6f0b666f1e3cf482b2b2e74fdacb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-03 17:59:44 +0800

    chore: add getServerSideProps withSession

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from 'next'
+import { GetServerSideProps } from 'next'
+import withSession from '~/lib/Session'
 import React from 'react'
 import Head from 'next/head'
 import Layout from '~/layouts/default'
@@ -16,5 +18,22 @@ const Home: NextPage = () => {
     </React.Fragment>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = withSession(async function (context: any) {
+  const user = context.req.session.get('user')
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+})
 
 export default Home
