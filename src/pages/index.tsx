1723a5409c5533fea3eb62b95aefea6dc71506ae
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-04 14:15:53 +0800

    refactor: change ssr to sg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,36 @@
-import type { NextPage } from 'next'
-import { GetServerSideProps } from 'next'
-import withSession from '~/lib/Session'
+import type { GetStaticProps, NextPage } from 'next'
 import React from 'react'
 import Head from 'next/head'
+import useUser from '~/lib/useUser'
 import Layout from '~/layouts/default'
+import Guard from '~/layouts/guard'
 import NewsFeed from '~/components/NewsFeed'
 
 const Home: NextPage = () => {
+
+  const { user: host } = useUser()
+
+  if (!host || host.isLoggedIn === false) {
+    return (
+      <React.Fragment>
+        <Head>
+          <title>FixRhythm</title>
+        </Head>
+        <Guard />
+      </React.Fragment>
+    )
+  }
+
   return (
     <React.Fragment>
       <Head>
         <title>Fixrhythm</title>
       </Head>
-      <Layout>
+      <Layout host={host}>
         <NewsFeed />
       </Layout>
     </React.Fragment>
   )
 }
-
-export const getServerSideProps: GetServerSideProps = withSession(async function (context: any) {
-  const user = context.req.session.get('user')
-
-  if (!user) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false
-      }
-    }
-  }
-
-  return {
-    props: {}
-  }
-})
 
 export default Home
