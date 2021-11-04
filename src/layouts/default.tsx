0508e3fd0e5d16355517df76a5ab69f53beb3245
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-04 14:13:37 +0800

    chore: add host props

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -4,16 +4,17 @@ import LeftSideBar from '~/components/Sidebars/LeftSideBar'
 import RightSideBar from '~/components/Sidebars/RightSideBar'
 
 interface TypeProps {
+  host: any
   children: any
 }
 
-const Layout: React.FC<TypeProps> = ({ children }) => {
+const Layout: React.FC<TypeProps> = ({ host, children }) => {
   return (
     <div className="preventcopy font-poppins flex justify-center w-full h-screen bg-pantone-darkblack overflow-hidden">
       <div className="relative flex flex-col w-full max-w-[2400px] h-full text-pantone-white bg-pantone-black bg-opacity-50">
         <NavigationBar />
         <div className="flex flex-row items-start w-full max-w-full h-full">
-          <LeftSideBar />
+          <LeftSideBar host={host} />
           <div className="flex flex-col w-full max-w-full h-full overflow-y-auto">
             { children }
           </div>
