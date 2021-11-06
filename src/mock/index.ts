89ccb4663568c2619349f663bbd1ea851472ac6d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-06 14:54:07 +0800

    chore: update mock data

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -2,21 +2,25 @@ export const artists = [
   {
     profile: 'https://pbs.twimg.com/media/EdNSO5JXkAA9ch4.jpg',
     name: 'Lisa Manoban',
+    respects: '80M',
     type: 'Poet'
   },
   {
     profile: 'https://i.pinimg.com/474x/bd/1c/27/bd1c275b4b1c8e9672c15a5d18505931.jpg',
     name: 'Roseanne Park',
+    respects: '50M',
     type: 'Lyricist'
   },
   {
     profile: 'https://i.pinimg.com/236x/a6/43/49/a64349c7add3a4ad7cb81c2cff149af5.jpg',
     name: 'Jennie Kim',
+    respects: '75.1M',
     type: 'Lyricist'
   },
   {
     profile: 'https://64.media.tumblr.com/24b6661d8facb7c27b41fd21e1655005/7c830f37b65ef339-64/s1280x1920/ff00091b0a4c5ab424cc17ee56ebdc63851c0215.jpg',
     name: 'Kim Jisoo',
+    respects: '50.1M',
     type: 'Poet'
   }
 ]
