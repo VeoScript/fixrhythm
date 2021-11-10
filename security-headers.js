2bc10046afb56943f307141e6a9014461994a832
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-10 12:29:00 +0800

    feat: add nextjs security-headers

---
+++ security-headers.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'Server',
+    value: 'Apache', // phony server value
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'sameorigin',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'geolocation=*', // allow specified policies here
+  },
+];
