21d18581a81c3202079291c29e55878c3145262e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-12 08:59:39 +0800

    style: fix profile page posts display full-width (#27)

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -19,17 +19,21 @@ const Layout: React.FC<TypeProps> = ({ host, artists, children }) => {
       <div className="relative flex flex-col w-full max-w-[2400px] h-full text-pantone-white bg-pantone-darkblack bg-opacity-50">
         <NavigationBar host={host} />
         <div className="flex flex-row items-start w-full max-w-full h-full">
-          {!(pathname === '/[username]' || pathname === '/[username]/followers' || pathname === '/[username]/following') && (
+          {!(pathname === '/[username]' || pathname === '/[username]/[title]' || pathname === '/[username]/followers' || pathname === '/[username]/following') && (
             <LeftSideBar host={host} />
           )}
           <div className="flex flex-col w-full max-w-full h-full overflow-y-auto">
             { children }
           </div>
           {!host || host.isLoggedIn === true && (
-            <RightSideBar
-              host={host}
-              artists={artists}
-            />
+            <React.Fragment>
+              {pathname !== '/[username]/posts/[uuid]' && (
+                <RightSideBar
+                  host={host}
+                  artists={artists}
+                />
+              )}
+            </React.Fragment>
           )}
         </div>
       </div>
