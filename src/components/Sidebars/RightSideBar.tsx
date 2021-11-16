03ede5150a183065f76a2da95df59391a1aebee9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-16 11:44:44 +0800

    feat: phase 12 - add display user bookmarks component (#31)
    * feat: add display user bookmarks component
    
    * feat: add display suggested artists component
    
    * refactor: add tab component for display suggested artists and bookmarks
    
    * chore: check url query username if equals to the owner of the post
    
    * chore: add bookmarks details in user api-route
    
    * style: add text-center class in follow and unfollow button

--- src/components/Sidebars/RightSideBar.tsx
+++ src/components/Sidebars/RightSideBar.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import Link from 'next/link'
 import useSWR from 'swr'
-import FollowButton from '../Interactions/Follows/FollowButton'
+import DisplayBookmarks from '../DisplayBookmarks'
+import DisplaySuggestedArtists from '../DisplaySuggestedArtists'
+import { Tab } from '@headlessui/react'
 
 const fetcher = async (
   input: RequestInfo,
@@ -18,6 +18,10 @@ interface TypeProps {
   artists: any
 }
 
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 const RightSideBar: React.FC<TypeProps> = ({ host, artists }) => {
 
   const { data: fetchArtists } = useSWR('/api/artists', fetcher, {
@@ -26,62 +30,49 @@ const RightSideBar: React.FC<TypeProps> = ({ host, artists }) => {
   })
 
   return (
-    <div className="flex flex-col w-full max-w-xs h-full overflow-y-auto bg-pantone-gray border-l border-pantone-white border-opacity-10">
-      <div className="flex flex-col items-center w-full">
-        <div className="flex items-center w-full px-3 py-2 space-x-1 border-b border-pantone-white border-opacity-10">
-          <div className="flex items-center justify-between w-full space-x-1">
-            <span className="font-normal text-xs text-pantone-white text-opacity-50">Suggested Accounts</span>
-            <Link href="/">
-              <a className="font-extralight text-[10px] text-pantone-white text-opacity-50 hover:underline">See All</a>
-            </Link>
-          </div>
-        </div>
-        {fetchArtists.map((artist: any, i: number) => {
-          const check_follow = artist.followedBy.some((follow: { followingId: any }) => follow.followingId === host.uuid)
-          return (
-            <React.Fragment key={i}>
-              {/* if the loggedin username is equal to the artist username it will not appear to the list, you cant respect your own account. */}
-              {host.username !== artist.username && (
-                <React.Fragment>
-                  {/* if the users/artists are already followed by the host, this will no longer display in suggested accounts */}
-                  {!check_follow && (
-                    <div className="flex flex-row items-center justify-between w-full p-3 space-x-2 border-b border-pantone-white border-opacity-10">
-                      <Link href={`${`/${ artist.username }`}`}>
-                        <a className="flex flex-row items-center space-x-2">
-                          <img
-                            className="w-10 h-10 rounded-full object-cover bg-pantone-darkblack"
-                            src={`${ !artist.profile ? `https://ui-avatars.com/api/?name=${ artist.name }&background=1D1F21&color=aaa` : artist.profile }`}
-                            alt=""
-                          />
-                          <div className="flex flex-col">
-                            <span className="font-bold text-sm text-pantone-white hover:underline">{ artist.name }</span>
-                            <div className="flex items-end space-x-2">
-                              <span className="font-light text-xs text-pantone-white text-opacity-80">{ artist.account_type }</span>
-                              <span className="font-light text-[10px] text-pantone-white text-opacity-30">{ artist.followedBy.length } Followers</span>
-                            </div>
-                          </div>
-                        </a>
-                      </Link>
-                      {/* if you are logged in your account will not display in suggestion accounts */}
-                      {host.username !== artist.username && (
-                        <React.Fragment>
-                          {/* if you already followed the users/artists this will be no longer display in suggestion accounts */}
-                          {!check_follow && (
-                            <FollowButton
-                              host={host}
-                              profile={artist}
-                              className="follow_button flex justify-center font-normal text-[10px] text-center px-2 py-1 rounded-lg bg-pantone-darkblack text-pantone-white transition ease-linear duration-200 hover:bg-pantone-white hover:bg-opacity-10"
-                            />
-                          )}
-                        </React.Fragment>
-                      )}
-                    </div>
-                  )}
-                </React.Fragment>
-              )}
-            </React.Fragment>
-          )
-        })}
+    <div className="flex flex-col w-full max-w-xs h-full overflow-x-hidden overflow-y-auto bg-pantone-gray border-l border-pantone-white border-opacity-10">
+      <div className="flex flex-col items-center w-full max-w-full">
+        <Tab.Group>
+          <Tab.List>
+            <div className="flex flex-row items-center w-full space-x-0.5 border-b border-pantone-white border-opacity-5">
+              <Tab
+                className={({ selected }) => 
+                  classNames(
+                    'w-[10rem] text-sm transition ease-linear duration-200',
+                    selected
+                      ? 'font-normal text-xs p-2 bg-pantone-white bg-opacity-10 text-pantone-white transition ease-linear duration-200 hover:bg-pantone-gray'
+                      : 'font-normal text-xs p-2 bg-pantone-black text-pantone-white transition ease-linear duration-200 hover:bg-pantone-gray'
+                  )
+                }
+              >
+                Suggested Accounts
+              </Tab>
+              <Tab
+                className={({ selected }) => 
+                  classNames(
+                    'w-[10rem] text-sm transition ease-linear duration-200',
+                    selected
+                      ? 'font-normal text-xs p-2 bg-pantone-white bg-opacity-10 text-pantone-white transition ease-linear duration-200 hover:bg-pantone-gray'
+                      : 'font-normal text-xs p-2 bg-pantone-black text-pantone-white transition ease-linear duration-200 hover:bg-pantone-gray'
+                  )
+                }
+              >
+                My Bookmarks
+              </Tab>
+            </div>
+          </Tab.List>
+          <Tab.Panels className="flex w-full">
+            <Tab.Panel className="flex w-full">
+              <DisplaySuggestedArtists
+                host={host}
+                fetchArtists={fetchArtists}
+              />
+            </Tab.Panel>
+            <Tab.Panel className="flex w-full">
+              <DisplayBookmarks host={host} />
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
       </div>
     </div>
   )
