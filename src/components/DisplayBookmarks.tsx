93151234a1e95b8b87999bad9c4fb74f50d59b9c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-16 11:40:50 +0800

    feat: add display user bookmarks component

---
+++ src/components/DisplayBookmarks.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'next/link'
+import ReactionButton from './Interactions/ReactionButton'
+import BookmarkButton from './Interactions/BookmarkButton'
+import { RiMusic2Fill, RiBookOpenFill } from 'react-icons/ri'
+
+interface TypeProps {
+  host: any
+}
+
+const DisplayBookmarks: React.FC<TypeProps> = ({ host }) => {
+  return (
+    <div className="flex flex-col w-full pb-16">
+      {host.bookmarks.map((bookmark: any, i: number) => (
+        <div className="flex flex-row items-center justify-between w-full space-x-2 border-b border-pantone-white border-opacity-5" key={i}>
+          <div className="flex flex-col w-full p-3 space-y-2">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-1">
+                {bookmark.composition.category === 'Song' ? <RiMusic2Fill className="w-4 h-4 text-pantone-white text-opacity-30" /> : <RiBookOpenFill className="w-4 h-4 text-pantone-white text-opacity-30" />}
+                <Link href={`/${bookmark.composition.user.username}/posts/${bookmark.composition.slug}`}>
+                  <a className="font-bold text-sm hover:underline">{ bookmark.composition.title }</a>
+                </Link>
+              </div>
+              <div className="flex items-center space-x-2">
+                <ReactionButton
+                  host={host}
+                  composition={bookmark.composition}
+                />
+                <BookmarkButton
+                  host={host}
+                  composition={bookmark.composition}
+                />
+              </div>
+            </div>
+            <span className="flex w-full font-light text-xs text-pantone-white text-opacity-50">{ bookmark.composition.description }</span>
+            <div className="flex items-center space-x-1">
+              <span className="font-medium text-[11px]">{ bookmark.composition.user.name }</span>
+              <span className="font-light text-[10px]">- { bookmark.composition.user.account_type }</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default DisplayBookmarks
