b9458591d484bbeaa6c5db3a9928f2cb005c4e83
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-11-28 14:26:21 +0800

    feat: create api-routes for uploading profile and cover photos

---
+++ src/pages/api/account-settings/upload_profile/create.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getImage } from '~/lib/Formidable'
+import { uploadImage } from '~/lib/Cloudinary'
+import { withIronSession, Session } from 'next-iron-session'
+type NextIronRequest = NextApiRequest & { session: Session }
+import prisma from '~/lib/Prisma'
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+async function handler(
+  req: NextIronRequest,
+  res: NextApiResponse,
+): Promise<void> {
+
+  const user = req.session.get('user')
+
+  const imageUploaded = await getImage(req)
+
+  const imageData = await uploadImage(imageUploaded.path)
+
+  const result = await prisma.profile.create({
+    data: {
+      publicId: imageData.public_id,
+      format: imageData.format,
+      version: imageData.version.toString(),
+      userId: user.id
+    }
+  })
+
+  res.status(200).json(result)
+}
+
+export default withIronSession(handler, {
+  password: 'complex_password_at_least_32_characters_long',
+  cookieName: 'fixrhythm',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production'
+  }
+})
