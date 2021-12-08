533e0661ae9929e5ccf626d57f25d4bf076a16aa
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-12-08 20:22:52 +0800

    chore: initialize env variable

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ module.exports = {
   reactStrictMode: true,
   env: {
     CLOUD_NAME: process.env.CLOUD_NAME,
-    SERVER_PATH: process.env.SERVER_PATH
+    SERVER_PATH: process.env.SERVER_PATH,
+    JWT_SECRET: process.env.JWT_SECRET,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
   },
   // append this at the bottom of your next.config.js file
   async headers() {
