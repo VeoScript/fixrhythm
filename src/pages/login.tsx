8b57c559b4c2704778d5472c54e82339bb621926
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-12-09 12:44:14 +0800

    chore: update SEO properties (#88)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,11 +10,11 @@ const Login: NextPage = () => {
     <React.Fragment>
       <Head>
         <title>Login | Fixrhythm</title>
-        <meta name="description" content="Fixrhythm Login" />
-        <meta name="og:title" content="Fixrhythm" />
-        <meta name="og:description" content="Fixrhythm Lyricist and Poet Social Media" />
-        <meta name="og:url" content="https://www.fixrhythm.tk/login" />
-        <meta name="og:type" content="website" />
+        <meta name="description" content="Fixrhythm Lyricist and Poet Social Media" />
+        <meta property="og:title" content="Fixrhythm" />
+        <meta property="og:description" content="Fixrhythm Lyricist and Poet Social Media" />
+        <meta property="og:url" content="https://www.fixrhythm.tk/login" />
+        <meta property="og:type" content="website" />
       </Head>
       <Auth>
         <LoginForm />
