5ea7116feff9d262541aa690e3026bdb023c07eb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-02-05 11:23:05 +0800

    feat: add about page and contents (#109)
    * feat: add about and github source code links to login and signup page
    
    * feat: add about page and content
    
    * feat: add pantone mock data
    
    * feat: add pantone official logo

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import Link from 'next/link'
+import { RiInformationLine, RiGithubFill } from 'react-icons/ri'
 
 interface TypeProps {
   children: any
@@ -8,8 +10,22 @@ interface TypeProps {
 const Auth: React.FC<TypeProps> = ({ children }) => {
   return (
     <div className="preventcopy font-poppins flex flex-col items-center justify-start md:justify-center w-full h-screen bg-pantone-white dark:bg-pantone-black overflow-hidden">
-      <div className="flex flex-col items-center justify-start md:justify-center w-full max-w-[2400px] h-full overflow-y-auto md:overflow-y-hidden py-10 md:py-0 text-pantone-darkblack dark:text-pantone-white bg-pantone-white dark:bg-pantone-darkblack">
+      <div className="relative flex flex-col items-center justify-start md:justify-center w-full max-w-[2400px] h-full overflow-y-auto md:overflow-y-hidden py-10 md:py-0 text-pantone-darkblack dark:text-pantone-white bg-pantone-white dark:bg-pantone-darkblack">
         <div className="flex flex-col items-center w-full space-y-5">
+          <div className="absolute top-5 right-5">
+            <div className="flex flex-row items-center space-x-2">
+              <Link href="/about">
+                <a title="About">
+                  <RiInformationLine className="w-5 h-5 text-pantone-gray dark:text-pantone-white transition ease-in-out duration-200 hover:scale-95" />
+                </a>
+              </Link>
+              <Link href="https://github.com/VeoScript/fixrhythm" passHref={true}>
+                <a title="Project Source" target="_blank">
+                  <RiGithubFill className="w-5 h-5 text-pantone-gray dark:text-pantone-white transition ease-in-out duration-200 hover:scale-95" />
+                </a>
+              </Link>
+            </div>
+          </div>
           <div className="flex flex-col items-center w-full">
             <h3 className="font-black text-3xl text-pantone-red">FIXRHYTHM</h3>
             <h6 className="font-light text-sm text-center">Evolve Your Ideas</h6>
---
+++ src/mock/pantone.ts
@@ -0,0 +1,17 @@
+export const pantone = [
+  {
+    color: 'bg-[#24282B]',
+    code: '426 XGC',
+    name: ''
+  },
+  {
+    color: 'bg-[#EDF1FF]',
+    code: '11-4001 TCX',
+    name: 'Brilliant White'
+  },
+  {
+    color: 'bg-[#C71F2D]',
+    code: '18-1763 TCX',
+    name: 'High Risk Red'
+  },
+]
