129b116c8895c6c52f36fb28f427ccd6c40f3fd1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-02-05 09:59:43 +0800

    style: update page 404 light mode display

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,11 +8,11 @@ const Custom404: NextPage = () => {
       <Head>
         <title>404 | Fixrhythm</title>
       </Head>
-      <div className="font-poppins flex flex-row items-center justify-center w-full h-screen text-pantone-white bg-pantone-darkblack">
+      <div className="font-poppins flex flex-row items-center justify-center w-full h-screen text-pantone-darkblack dark:text-pantone-white bg-pantone-white dark:bg-pantone-darkblack">
         <div className="flex items-center justify-center w-full space-x-5">
           <h1 className="font-bold text-xl text-red-600">404</h1>
-          <hr className="border-l border-pantone-white border-opacity-10 h-16" />
-          <h2 className="font-normal text-sm text-pantone-white text-opacity-50">This page could not be found.</h2>
+          <hr className="border-l border-pantone-gray dark:border-pantone-white border-opacity-10 dark:border-opacity-10 h-16" />
+          <h2 className="font-normal text-sm text-pantone-darkblack dark:text-pantone-white text-opacity-80 dark:text-opacity-50">This page could not be found.</h2>
         </div>
       </div>
     </React.Fragment>
